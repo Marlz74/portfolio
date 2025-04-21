d811b27433dbe7c3341a5248353db7b4ad9942a2
SD <utpatrick74@gmail.com>
2025-04-21 21:13:16 +0100

    Update: Updated project images

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Title from '../layouts/Title'
 import ProjectsCard from './ProjectsCard'
-import projectOne from '/images/projectOne.jpg'
+import api from '/images/api.png'
+import jobwings from '/images/jobwings.jpeg'
+import kreater from '/images/kreater.jpeg'
+import phartse from '/images/phartse.jpeg'
+import rigitix from '/images/rigitix.jpeg'
+import telegram from '/images/telegram.png'
 
 
 const Projects = () => {
   return (
-    
+
     <section
       id="projects"
       className="w-full py-20 border-b-[1px] border-b-black"
@@ -21,40 +26,44 @@ const Projects = () => {
         <ProjectsCard
           title="Jobwings"
           des="A platform where recruiters and job seekers connect to apply for jobs."
-          src="https://jobwings33.com/"
+          src={jobwings}
           websiteLink="https://jobwings33.com/"
         />
 
         <ProjectsCard
           title="rigitiX"
           des="An event management software that allows event owners to sell tickets and get real-time updates on ticket sales."
-          src="https://rigitix.com/"
+          src={rigitix}
           websiteLink="https://rigitix.com/"
         />
+
         <ProjectsCard
           title="Kreater"
           des="A logistics platform for shipping goods from China and Bangladesh to Nigeria with automated shipment tracking."
-          src="https://kreater.africa/"
+          src={kreater}
           websiteLink="https://kreater.africa/"
         />
 
         <ProjectsCard
           title="Pharste Fintech App"
           des="A fintech app for currency exchange and P2P transactions."
-          src="https://pharste.com/"
+          src={phartse}
         />
+
         <ProjectsCard
           title="Custom MVC API Template"
           des="A PHP-based API template for custom MVC applications."
-          src="https://github.com/Marlz74/api_template_php"
+          src={api}
           githubLink="https://github.com/Marlz74/api_template_php"
         />
+
         <ProjectsCard
           title="Telegram MiniApp Backend"
           des="A backend for a Telegram mini app built using Node.js."
-          src="https://github.com/Marlz74/telegram_bot"
+          src={telegram}
           githubLink="https://github.com/Marlz74/telegram_bot"
         />
+
       </div>
     </section>
   );
